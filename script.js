f9f36042458d4c611a6127b3a7dced77aa81239c
Bradley Jester <bradleyjesterold@Brians-MacBook-Pro.local>
2022-08-29 15:21:12 -0700

    added pseduocode

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Hey! You clicked the button!");
+
+// 1. Prompt the user fot the password criteria
+//    a. Password length 8 < 128
+//    b. lowercase, uppercase, numbers, special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+
+// 4. Display password to the page.
+  return "Generated password will go here!";
+}
 
 // Write password to the #password input
 function writePassword() {
