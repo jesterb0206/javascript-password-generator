015b3534b9951ad2338ca6d5ffebadbecf3f7484
Bradley Jester <bradleyjesterold@brians-mbp.lan>
2022-08-31 18:21:29 -0700

    Prompts the user for the password criteria

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  console.log("Hey! You clicked the button!");
 
-// 1. Prompt the user fot the password criteria
+  // Prompts the user for the password criteria
+  var userInput = window.prompt("How many characters do you want your password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("Please enter a number!")
+    return
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Your password must be between 8 and 128 characters!")
+    return
+  }
+
+  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
+  var userWantsNumeric = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSpecialCharacters = window.confirm("Would you like to include special characters in your password?")
+
+// 1. Prompt the user for the password criteria
 //    a. Password length 8 < 128
 //    b. lowercase, uppercase, numbers, special characters
 // 2. Validate the input
