9f2334b0380bd36684675919e9a5a92fbd0f072b
Bradley Jester <bradleyjesterold@brians-mbp.lan>
2022-08-31 22:05:03 -0700

    Fully Working Password Generator

--- script.js
+++ script.js
@@ -1,46 +1,96 @@
-// Assignment code here
 var generateBtn = document.querySelector("#generate");
+
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getRandomCharacter(list) {
+  return list[randomInt(list.length)]
+}
 
 function generatePassword() {
 
-  // Prompts the user for the password criteria
-  var userInput = window.prompt("How many characters do you want your password to be?")
+  while (true) {
 
-  var passwordLength = parseInt(userInput)
+    var userInput = window.prompt("How many characters do you want your password to be?")
 
-  if (isNaN(passwordLength)) {
-    window.alert("Please enter a number!")
-    return
+    if (userInput === null) {
+      return
+    }
+
+    var passwordLength = parseInt(userInput)
+
+    if (isNaN(passwordLength)) {
+      window.alert("Please enter a number!")
+    } else if (passwordLength < 8 || passwordLength > 128){
+      window.alert("Invalid password length. The password length should be between 8 and 128 characters")
+    } else {
+      break
+    }
+
   }
 
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Your password must be between 8 and 128 characters!")
-    return
-  }
-
-  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
-  var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
   var userWantsNumeric = window.confirm("Would you like to include numbers in your password?")
   var userWantsSpecialCharacters = window.confirm("Would you like to include special characters in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?")
 
-// 1. Prompt the user for the password criteria
-//    a. Password length 8 < 128
-//    b. lowercase, uppercase, numbers, special characters
-// 2. Validate the input
-// 3. Generate password based on criteria
+  var numeric = [0,1,2,3,4,5,6,7,8,9]
+  var specialCharacters = ["!", "”", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+  var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var uppercase = []
 
-// 4. Display password to the page.
-  return "Generated password will go here!";
+  var characterType = []
+
+  for (var i = 0; i <lowercase.length; i++) {
+     uppercase[i] = lowercase[i].toUpperCase()
+  } 
+
+  if (userWantsLowercase === true) {
+    characterType.push(lowercase)
+  }
+
+  if (userWantsUppercase === true) {
+    characterType.push(uppercase)
+  }
+
+  if (userWantsNumeric === true) {
+    characterType.push(numeric)
+  }
+
+  if (userWantsSpecialCharacters === true) {
+    characterType.push(specialCharacters)
+  }
+
+  if (characterType.length === 0) {
+    characterType.push(specialCharacters)
+  }
+  
+  var generatedPassword = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomList = getRandomCharacter(characterType)
+    var randomChar = getRandomCharacter(randomList)
+    generatedPassword += randomChar
+  } 
+
+  return generatedPassword
+
 }
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (password) {
+    passwordText.value = password;
+  }
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
